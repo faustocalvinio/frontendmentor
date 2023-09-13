06a5682f2afe2671673138b0fd718935ddecd094
Fausto Calviño <faustocalvino@outlook.com>
2023-09-13 19:59:24 +0200

    Starting easybank landing page challenge

--- ACTIVE-intro-section-with-dropdown-navigation/scripts/main.js
+++
@@ -1,7 +0,0 @@
-const featuresLink=document.getElementById('features');
-const featuresDropdown=document.querySelector('.features-dropdown')
-
-
-featuresLink.addEventListener('mouseover',()=>{
-    featuresDropdown.classList.toggle('hide')
-})
