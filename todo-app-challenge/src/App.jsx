4321e0c1c3ae82acbfc7db24a6689bc0e4d82488
Fausto Calviño <faustocalvino@outlook.com>
2023-10-03 18:04:33 +0200

    feat developing all features

--- todo-app-challenge/src/App.jsx
+++ todo-app-challenge/src/App.jsx
@@ -1,14 +1,26 @@
 import { useContext, useEffect, useRef, useState } from "react";
 import { TodosContext } from "./context/todosContext";
 import BgImage from '../images/bg-desktop-light.jpg';
+import crossIcon from '../images/icon-cross.svg';
+import moonIcon from '../images/icon-moon.svg';
+import sunIcon from '../images/icon-sun.svg';
+
+
+
+
 function App() {
-  const { todos, addTodo, deleteTodo, toggleTodo, emptyTodos } = useContext(TodosContext);
+  const { todos, addTodo, deleteTodo,  itemsLeft , toggleTodo, emptyTodos, clearCompleted } = useContext(TodosContext);
+  console.log(itemsLeft);
   console.log(todos);
   const textInput = useRef()
   useEffect(() => {
     
   }, [todos])
   
+  const onClearCompleted=()=>{
+    clearCompleted()
+    
+  }
 
   const submitTodoAction = (e) => {
     e.preventDefault();
@@ -18,35 +30,66 @@ function App() {
     addTodo({
       id: date,
       title:textInput.current.value,
+      done:false
     });
     textInput.current.value='';
   }
 
+  const onToggleTodo=(e,id)=>{
+    console.log(e.target.checked);
+    toggleTodo(id)
+  }
 
   const deleteTodoAction=(id)=>{
     console.log(id);
     deleteTodo(id);
   }
+
+
   return (
     <div className="flex flex-col justify-center min-h-full">
       <img src={BgImage} className="absolute top-0 w-full" alt="" />
-      <main className="z-10 w-[900px] border-2 border-red-600 mx-auto flex flex-col gap-10">
+      <main className="min-h-[80vh] z-10 w-[900px] border-2 border-red-600 mx-auto flex flex-col gap-10">
+        <div className="flex justify-between">
+        <h1 className="text-5xl text-left text-white font-semibold tracking-wider">TODO</h1>
+        <button id="theme-toggle">
+          <img className='h-auto w-12' id="moon-icon" src={moonIcon} alt="" />
+          <img className='h-auto w-12' id="sun-icon" src={sunIcon} alt="" />
+        </button>
+        
+        </div>
+
         <form action="" onSubmit={ submitTodoAction } >
+       
           <input className="w-full py-4 px-2 rounded-lg" placeholder="Create new TODO..." type="text" ref={textInput} />
+
         </form>
+
         <button className="bg-gray-400" onClick={emptyTodos}>Empty TODOS</button>
-        <h1 className="text-5xl text-center text-white font-semibold tracking-wider">TODO</h1>
+
         <ul className="flex flex-col ">
             {
               todos.map((todo) => (
-                <li className="bg-red-300 flex justify-between border-b-2 border-gray-700 py-2" key={todo.id}>
-                  <span className="text-3xl">{todo.title}</span>
-                  <button onClick={()=>deleteTodoAction(todo.id)}> delete todo</button>
+                <li className="relative bg-white px-4 flex justify-start border-b-[1px] border-gray-300 py-2" key={todo.id}>
+                  <input checked={todo.done} onChange={(e)=>onToggleTodo(e,todo.id)} className="w-10 rounded-3xl" type="checkbox" name="" id=""/>
+                  <span className={todo.done ? "line-through text-gray-500 text-3xl mx-4" : "text-3xl mx-4"}>{todo.title}</span>
+                 
+                  <button className="absolute right-4 bg-blue-200 px-4 py-2 rounded-lg" onClick={()=>deleteTodoAction(todo.id)}><img className="w-6 h-6 invert" src={crossIcon}></img></button>
                   
-                  </li>
+                </li>
               ))
             }
         </ul>
+        
+        <div className="flex flex-row justify-between px-2">
+          <span>{itemsLeft} items left</span>
+          <ul className="flex flex-row gap-2">
+            <li>All</li>
+            <li>Active</li>
+            <li>Completed</li>
+          </ul>
+            <button className="bg-gray-400 px-4 py-2 rounded-lg" onClick={onClearCompleted}>clear completed</button>
+        </div>
 
       </main>
     </div>
--- todo-app-challenge/src/context/todosContext.jsx
+++ todo-app-challenge/src/context/todosContext.jsx
@@ -1,45 +1,75 @@
-import { createContext } from 'react'
+import { createContext, useEffect } from 'react'
 import { useState } from 'react';
 
-const initialTodos=[
-    {id:1,title: 'Learn React'},
-    {id:2,title: 'Learn HTML'},
-    {id:3,title: 'Learn CSS'},
-    {id:4,title: 'Learn JavaScript'},
-    {id:5,title: 'Learn NextJS'},
-    {id:6,title: 'Learn TailwindCSS'},
-    {id:7,title: 'Learn Git'},
-
+const initialTodosBase=[
+    {id:1,title: 'Complete online JavaScript course',done:true},
+    {id:2,title:'Jag around the park 3x',done:false},
+    {id:3,title: '10 minutes meditation',done:false},
+    {id:4,title: 'Read for 1 hour',done:false},
+    {id:5,title: 'Pick up groceries',done:false},
+    {id:6,title: 'Complete Todo App on Frontend Mentor',done:false},
 ];
 
 export const TodosContext = createContext();
 
 export function TodosProvider({ children }) {
-    const [todos, setTodos] = useState(initialTodos);
 
+  
+    
+    const [todos, setTodos] = useState( JSON.parse(localStorage.getItem('todos')) || initialTodosBase);
+    const [itemsLeft, setItemsLeft] = useState()
+
+    const itemsLeftCount = () => {
+        const count = todos.filter((todo) => !todo.done).length;
+        setItemsLeft(count);
+    }
+
+    useEffect(() => {
+        itemsLeftCount()
+    },[todos])
 
     const  addTodo = (todo) =>{
         setTodos([...todos, todo]);
+        localStorage.setItem('todos', JSON.stringify(todos));
     }
 
     const deleteTodo = (id) =>{
         const newsTodos = todos.filter((todo) => todo.id !== id);
-        setTodos(newsTodos);    
+        setTodos(newsTodos);  
+        localStorage.setItem('todos', JSON.stringify(todos));  
     }
 
     const toggleTodo = (id) => {
+        const newsTodos = todos.map((todo) => {
+            if (todo.id === id) {
+                return { ...todo, done: !todo.done };
+            }
+            return todo;
+        
+        });
         console.log(id);
+        setTodos(newsTodos);
+        localStorage.setItem('todos', JSON.stringify(todos));
     } 
 
     const emptyTodos=()=>{
         setTodos([]);
     }
+
+    const clearCompleted=()=>{     
+        
+        const newTodos = todos.filter((todo) => !todo.done);        
+        setTodos(newTodos);
+    }
+
     return (
         <TodosContext.Provider
             value={{
                 addTodo,
+                clearCompleted,
                 deleteTodo,
                 emptyTodos,
+                itemsLeft,
                 todos,
                 toggleTodo,
             }}
